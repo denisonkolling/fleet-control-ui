3c6bcc7132335bd5e38b12efa4fea06de18df328
Denison Kolling <denisonkolling@gmail.com>
2023-12-15 12:46:06 -0300

    style: modify service order list page

--- src/pages/ListRepairOrder.jsx
+++ src/pages/ListRepairOrder.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import repairOrderService from '../service/repairOrder.service';
 import NavbarSystem from '../components/Navbar';
 import { Card, CardGroup, Container, ModalTitle } from 'react-bootstrap';
-import CardHeader from 'react-bootstrap/esm/CardHeader';
+import moment from 'moment';
 
 const ListRepairOrder = () => {
 	const [repairOrderList, setRepairOrderList] = useState([]);
@@ -27,21 +27,38 @@ const ListRepairOrder = () => {
 		<>
 			<NavbarSystem />
 			<Container>
-				<h1>Service Orders List</h1>
+				<h1>Service Orders</h1>
+				<div
+					style={{
+						display: 'grid',
+						gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+						gap: '1rem',
+						alignItems: 'flex-start',
+					}}>
 					{repairOrderList.map((o) => (
-				<CardGroup className='m-2'>
-        <Card>
-						<tr>
-							<CardHeader>Service Order Number: {o.workOrderId}</CardHeader>
-							<h6>Vehicle: {o.plate}</h6>
-							<span>Date: {o.openDate}</span><br/>
-							<span>Service: {o.service.serviceName} U$ {o.service.servicePrice}</span>
-              <br/>
-							<span>Part: {o.part.partName} U$ {o.part.partPrice}</span>
-						</tr>
-          </Card>
-				</CardGroup>
+						<CardGroup key={o.workOrderId} className="m-2">
+							<Card style={{backgroundColor: "#F8F9FA"}}>
+								<div>
+									<div className="d-flex justify-content-between align-items-baseline fw-normal mb-3 m-2">
+										<Card.Title className="">Service Order {o.workOrderId}</Card.Title>
+										<h5 className="">{o.plate}</h5>
+									</div>
+									<div className='m-2'>
+									<span>Open Date {moment(o.openDate).format("DD/MM/YYYY HH:mm")}</span>
+									<br />
+									<span>
+										Service {o.service.serviceName} U$ {o.service.servicePrice}
+									</span>
+									<br />
+									<span>
+										Part {o.part.partName} U$ {o.part.partPrice}
+									</span>
+									</div>
+								</div>
+							</Card>
+						</CardGroup>
 					))}
+				</div>
 			</Container>
 		</>
 	);

631040b041daaac6f56167f7151234409e11619b
Denison Kolling <denisonkolling@gmail.com>
2023-12-15 12:00:20 -0300

    style: modify home screen texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,19 +8,22 @@ const Home = () => {
 		<>
 			<NavbarSystem />
 			<Container>
-				<Card className="mt-3 col-4">
+				<Card className="mt-3 col-12">
 					<CardHeader className="text-center">
 						Welcome to Fleet Control Management System!
 					</CardHeader>
 					<h4 className='m-2'>
-						Tyres Control <Badge bg="secondary">New</Badge>
+						Tyres Control <Badge bg="secondary">New!</Badge>
 					</h4>
+					<p className='ms-2'>Check your fleet tyres performance</p>
 					<h4 className='m-2'>
-						Tip Expenses Control <Badge bg="secondary">New</Badge>
+						Tip Expenses Control <Badge bg="secondary">New!</Badge>
 					</h4>
+					<p className='ms-2'>Control your fleet expenses by budget</p>
 					<h4 className='m-2'>
-						Fuel Control <Badge bg="secondary">New</Badge>
+						Fuel Control <Badge bg="secondary">New!</Badge>
 					</h4>
+					<p className='ms-2'>Measure your drivers and vehicle fuel consumption</p>
 				</Card>
 			</Container>
 		</>

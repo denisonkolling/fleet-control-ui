7a3843257fc93d03060df2b434b9b12c406d35d7
Denison Kolling <denisonkolling@gmail.com>
2024-01-19 17:27:11 -0300

    feat: add error message vehicle registration

--- src/pages/AddVehicle.jsx
+++ src/pages/AddVehicle.jsx
@@ -15,6 +15,9 @@ const AddVehicle = () => {
 		year: '',
 	});
 
+	const [message, setMessage] = useState('');
+	const [error, setError] = useState('');
+
 	const [checked, setChecked] = useState(false);
 	const [radioValue, setRadioValue] = useState('1');
 
@@ -36,8 +39,10 @@ const AddVehicle = () => {
 			.saveVehicle(vehicle)
 			.then((res) => {
 				setVehicle({ plate: '', model: '', driver: '', mileage: '', year: '' });
+				setMessage('Vehicle added successfully!');
 			})
 			.catch((error) => {
+				setError('Something went wrong on server!');
 				console.log(error);
 			});
 	};
@@ -45,88 +50,92 @@ const AddVehicle = () => {
 	return (
 		<>
 			<NavbarSystem />
-			<Container className='mt-4 col-md-4'>
-			<Card>
-				<CardHeader className='text-center fs-4'>Vehicle Registration</CardHeader>
-				<Container className='p-4'>
-				<Form onSubmit={handleSubmit}>
-					<Form.Group className="mb-3" controlId="name">
-						<Form.Label>Registration Number</Form.Label>
-						<Form.Control
-							type="text"
-							name="plate"
-							onChange={handleChange}
-							value={vehicle.plate}
-							required
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="name">
-						<Form.Label>Model</Form.Label>
-						<Form.Control
-							type="text"
-							name="model"
-							onChange={handleChange}
-							value={vehicle.model}
-							required
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="name">
-						<Form.Label>Driver</Form.Label>
-						<Form.Control
-							type="text"
-							name="driver"
-							onChange={handleChange}
-							value={vehicle.driver}
-							required
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="name">
-						<Form.Label>Mileage</Form.Label>
-						<Form.Control
-							type="number"
-							name="mileage"
-							onChange={handleChange}
-							value={vehicle.mileage}
-							required
-						/>
-					</Form.Group>
-					<Form.Group className="mb-3" controlId="name">
-						<Form.Label>Year</Form.Label>
-						<Form.Control
-							type="number"
-							name="year"
-							onChange={handleChange}
-							value={vehicle.year}
-							required
-						/>
-					</Form.Group>
+			<Container className="mt-4 col-md-4">
+				<Card>
+					<CardHeader className="text-center fs-4">
+						Vehicle Registration
+					</CardHeader>
+					<Container className="p-4">
+						<Form onSubmit={handleSubmit}>
+							<Form.Group className="mb-3" controlId="name">
+								<Form.Label>Registration Number</Form.Label>
+								<Form.Control
+									type="text"
+									name="plate"
+									onChange={handleChange}
+									value={vehicle.plate}
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="name">
+								<Form.Label>Model</Form.Label>
+								<Form.Control
+									type="text"
+									name="model"
+									onChange={handleChange}
+									value={vehicle.model}
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="name">
+								<Form.Label>Driver</Form.Label>
+								<Form.Control
+									type="text"
+									name="driver"
+									onChange={handleChange}
+									value={vehicle.driver}
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="name">
+								<Form.Label>Mileage</Form.Label>
+								<Form.Control
+									type="number"
+									name="mileage"
+									onChange={handleChange}
+									value={vehicle.mileage}
+									required
+								/>
+							</Form.Group>
+							<Form.Group className="mb-3" controlId="name">
+								<Form.Label>Year</Form.Label>
+								<Form.Control
+									type="number"
+									name="year"
+									onChange={handleChange}
+									value={vehicle.year}
+									required
+								/>
+							</Form.Group>
 
-					<Form.Label>Fuel</Form.Label><br></br>
-					<ButtonGroup>
-						{radios.map((radio, idx) => (
-							<ToggleButton
-								key={idx}
-								id={`radio-${idx}`}
-								type="radio"
-								variant={idx % 2 ? 'outline-primary' : 'outline-primary'}
-								name="radio"
-								value={radio.value}
-								checked={radioValue === radio.value}
-								onChange={(e) => setRadioValue(e.currentTarget.value)}>
-								{radio.name}
-							</ToggleButton>
-						))}
-					</ButtonGroup>
+							<Form.Label>Fuel</Form.Label>
+							<br></br>
+							<ButtonGroup>
+								{radios.map((radio, idx) => (
+									<ToggleButton
+										key={idx}
+										id={`radio-${idx}`}
+										type="radio"
+										variant={idx % 2 ? 'outline-primary' : 'outline-primary'}
+										name="radio"
+										value={radio.value}
+										checked={radioValue === radio.value}
+										onChange={(e) => setRadioValue(e.currentTarget.value)}>
+										{radio.name}
+									</ToggleButton>
+								))}
+							</ButtonGroup>
 
-					<div className="d-flex justify-content-end mt-4">
-						<Button variant="primary" type="submit">
-							Save
-						</Button>
-					</div>
-
-				</Form>
-				</Container>
+							<div className="d-flex justify-content-end mt-4">
+								<Button variant="primary" type="submit">
+									Save
+								</Button>
+							</div>
+						</Form>
+					</Container>
 				</Card>
+				{message && <p className="fs-4 mt-2 text-center text-success">{message}</p>}
+				{error && <p className="fs-4 mt-2 text-center text-danger">{error}</p>}
 			</Container>
 		</>
 	);

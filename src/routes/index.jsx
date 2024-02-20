dc9a85d57dec78fe858d83922f868efda0a3b4b8
Denison Kolling <115804868+denisonkolling@users.noreply.github.com>
2024-02-20 16:28:34 +0000

    fix: routes signin page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -36,7 +36,7 @@ import { Fragment } from 'react';
 
 const Private = ({ Page }) => {
 	const { isLogged } = useState();
-	return !!isLogged ? <Page /> : <Login />;
+	return !!isLogged ? <Page /> : <Signin />;
 };
 
 const RoutesApp = () => {
@@ -44,40 +44,39 @@ const RoutesApp = () => {
 		<BrowserRouter>
 		<Fragment>
 			<Routes>
-			  <Route path="/" element={<Home />} />
-				<Route exact path="/home" element={<Home />} />
-				<Route exact path="/register-driver" element={<AddDriver />} />
-				<Route exact path="/register-customer" element={<AddCustomer />} />
-				<Route exact path="/register-vehicle" element={<AddVehicle />} />
-				<Route exact path="/register-tyre" element={<AddTyre />} />
-				<Route exact path="/register-invoice" element={<AddInvoice />} />
-				<Route exact path="/register-product" element={<AddProduct />} />
-				<Route exact path="/register-driving-hours" element={<AddDrivingHours />} />
-				<Route exact path="/register-service-order" element={<AddServiceOrder />} />
-				<Route exact path="/register-expense" element={<AddExpense />} />
-				<Route exact path="/register-trip" element={<AddTrip />} />
-				<Route exact path="/register-tyre-reading" element={<AddTyreReading />} />
-				<Route exact path="/vehicle-service-order" element={<ListRepairOrder />} />
-				<Route exact path="/vehicle-list" element={<ListVehicle />} />
-				<Route exact path="/vehicle-edit/:id" element={<EditVehicle />} />
-				<Route exact path="/driver-edit/:id" element={<EditDriver />} />
-				<Route exact path="/tyre-edit/:id" element={<EditTyre />} />
-				<Route exact path="/expense-list" element={<ListExpense />} />
-				<Route exact path="/tyre-list" element={<ListTyre />} />
-				<Route exact path="/tyre-reading" element={<ListTyreReading />} />
-				<Route exact path="/invoice-list" element={<ListInvoice />} />
-				<Route exact path="/customer-list" element={<ListCustomer />} />
-				<Route exact path="/vehicle-mileage" element={<ListMileage />} />
-				<Route exact path="/product-list" element={<ListProducts />} />
-				<Route exact path="/driver-list" element={<ListDriver />} />
-				<Route exact path="/trip-list" element={<ListTrip />} />
-				<Route exact path="/find-driver" element={<FindDriver />} />
-				<Route exact path="/find-expense" element={<FindExpense />} />
-				<Route exact path="/invoice-find" element={<FindInvoice />} />
-				<Route exact path="/vehicles-map" element={<VehiclesMap />} />
-				<Route exact path="/signup" element={<Signup />} />
 				<Route path="/" element={<Signin />} />
 				<Route path="*" element={<Signin />} />
+				<Route exact path="/signup" element={<Signup />} />
+				<Route exact path="/home" element={<Private Page={Home} />} />
+				<Route exact path="/register-driver" element={<Private Page={AddDriver} />} />
+				<Route exact path="/register-customer" element={<Private Page={AddCustomer} />} />
+				<Route exact path="/register-vehicle" element={<Private Page={AddVehicle} />} />
+				<Route exact path="/register-tyre" element={<Private Page={AddTyre} />} />
+				<Route exact path="/register-invoice" element={<Private Page={AddInvoice} />} />
+				<Route exact path="/register-product" element={<Private Page={AddProduct} />} />
+				<Route exact path="/register-driving-hours" element={<Private Page={AddDrivingHours} />} />
+				<Route exact path="/register-service-order" element={<Private Page={AddServiceOrder} />} />
+				<Route exact path="/register-expense" element={<Private Page={AddExpense} />} />
+				<Route exact path="/register-trip" element={<Private Page={AddTrip} />} />
+				<Route exact path="/register-tyre-reading" element={<Private Page={AddTyreReading} />} />
+				<Route exact path="/vehicle-service-order" element={<Private Page={ListRepairOrder} />} />
+				<Route exact path="/vehicle-list" element={<Private Page={ListVehicle} />} />
+				<Route exact path="/vehicle-edit/:id" element={<Private Page={EditVehicle} />} />
+				<Route exact path="/driver-edit/:id" element={<Private Page={EditDriver} />} />
+				<Route exact path="/tyre-edit/:id" element={<Private Page={EditTyre} />} />
+				<Route exact path="/expense-list" element={<Private Page={ListExpense} />} />
+				<Route exact path="/tyre-list" element={<Private Page={ListTyre} />} />
+				<Route exact path="/tyre-reading" element={<Private Page={ListTyreReading} />} />
+				<Route exact path="/invoice-list" element={<Private Page={ListInvoice} />} />
+				<Route exact path="/customer-list" element={<Private Page={ListCustomer} />} />
+				<Route exact path="/vehicle-mileage" element={<Private Page={ListMileage} />} />
+				<Route exact path="/product-list" element={<Private Page={ListProducts} />} />
+				<Route exact path="/driver-list" element={<Private Page={ListDriver} />} />
+				<Route exact path="/trip-list" element={<Private Page={ListTrip} />} />
+				<Route exact path="/find-driver" element={<Private Page={FindDriver} />} />
+				<Route exact path="/find-expense" element={<Private Page={FindExpense} />} />
+				<Route exact path="/invoice-find" element={<Private Page={FindInvoice} />} />
+				<Route exact path="/vehicles-map" element={<Private Page={VehiclesMap} />} />
 			</Routes>
 			</Fragment>
 		</BrowserRouter>
